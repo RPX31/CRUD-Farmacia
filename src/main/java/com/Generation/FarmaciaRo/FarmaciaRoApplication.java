cdf12ea80ca1d22abb6f98f0c1603a11233186d5
rodrigo oliveira <pracimaestudos@gmail.com>
2024-12-16 14:20:37 -0300

    configuração do properties

---
+++ src/main/java/com/Generation/FarmaciaRo/FarmaciaRoApplication.java
@@ -0,0 +1,13 @@
+package com.Generation.FarmaciaRo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaRoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaRoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Generation/FarmaciaRo/FarmaciaRoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Generation.FarmaciaRo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaRoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
